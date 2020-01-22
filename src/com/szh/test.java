ac37aa4655674bae149bc2132d12cff0d8fc14c0
szh <1910961676@qq.com>
2020-01-22 12:45:30 +0800

    将工程提交到本地仓库。

---
+++ src/com/szh/test.java
@@ -0,0 +1,13 @@
+package com.szh;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
+        System.out.println("hello5");
+        System.out.println("hello6");
+    }
+
+}
